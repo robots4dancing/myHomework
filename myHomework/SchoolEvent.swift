9c1af1ec41cc4745562eff6a015d294899e341b3
Valerie Greer <vagreer10@Valeries-MacBook-Pro.local>
2017-04-03 18:58:48 -0400

    Completed SchoolEvent model.

---
+++ myHomework/SchoolEvent.swift
@@ -0,0 +1,31 @@
+//
+//  schoolEvent.swift
+//  myHomework
+//
+//  Created by Valerie Greer on 4/3/17.
+//  Copyright © 2017 Shane Empie. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class SchoolEvent: PFObject, PFSubclassing {
+    
+    @NSManaged var schoolEventName          :String?
+    @NSManaged var schoolEventDescription   :String?
+    @NSManaged var schoolEventDueDate       :Date?
+    @NSManaged var schoolEventReminderDate  :Date?
+    @NSManaged var schoolEventStatus        :Bool?
+    
+    convenience init(eventName: String, eventDescription: String, eventDueDate: Date, eventReminderDate: Date, eventStatus: Bool) {
+        super.init()
+        
+        schoolEventName = eventName
+        schoolEventDescription = eventDescription
+        schoolEventDueDate = eventDueDate
+        schoolEventReminderDate = eventReminderDate
+        schoolEventStatus = eventStatus
+        
+    }
+
+}
